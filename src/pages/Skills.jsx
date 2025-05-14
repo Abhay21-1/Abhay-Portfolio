4f9a37104cead32b233b3fc1ba699eb9002f92d3
Abhay kaushal <abuabhay177112@gmail.com>
2025-05-14 17:27:10 +0530

    Updated Skills section

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -9,37 +9,37 @@ function Skills() {
           <h1>Skills</h1>
           <div className='row text-center mb-5'>
             <div className='col-sm-3 define-section'>
-              <img src='../public/html.svg'/>
+              <img src='/html.svg'/>
               <p>HTML</p>
             </div>
             <div className='col-sm-3 define-section'>
-              <img src='../public/css.svg'/>
+              <img src='/css.svg'/>
               <p>CSS</p>
             </div>
             <div className='col-sm-3 define-section1'>
-                <img src='../public/Bootstrap.svg'/>
+                <img src='/Bootstrap.svg'/>
                 <p>BOOTSTRAP</p>
             </div>
             <div className='col-sm-3 define-section'>
-              <img src='../public/javascript.svg'/>
+              <img src='/javascript.svg'/>
               <p>JAVASCRIPT</p>
             </div>
           </div>
           <div className='row text-center for-gap'>
             <div className='col-sm-3 define-section'>
-              <img src='../public/react.svg'/>
+              <img src='/react.svg'/>
               <p>REACT</p>
             </div>
             <div className='col-sm-3 define-section'>
-              <img src='../public/Node.js.svg'/>
+              <img src='/Node.js.svg'/>
               <p>NODE JS</p>
             </div>
             <div className='col-sm-3 define-section'>
-              <img src='../public/MongoDB.svg'/>
+              <img src='/MongoDB.svg'/>
               <p>MONGO DB</p>
             </div>
             <div className='col-sm-3 define-section'>
-              <img src='../public/Adobe Photoshop.svg'/>
+              <img src='/Adobe Photoshop.svg'/>
               <p>ADOBE PHOTOSHOP</p>
             </div>
           </div>
